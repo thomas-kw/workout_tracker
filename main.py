cc6bee370f0c70589a41f8efb07fa54d5fcac8e7
Thomas <whelan.thomas@gmail.com>
2021-10-13 17:04:24 +0900

    added api endpoint for nutritionix

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import requests
+
 APP_ID = "f457de60"
 API_KEY = "d8ca4caad74c8306f867d7fbd21caa4c"
 
-ypML0itxb4/VgyarRMmn0y3BZ3H2Zirb+1MjYp7f8G4
+nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+query = {
+    "query":"ran 20 miles",
+    "gender":"male",
+    "weight_kg":"50",
+}
+
+response = requests.post(url=nutritionix_endpoint, json=query, headers=headers)
+
+print(response.text)
