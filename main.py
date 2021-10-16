7ed2a65bcb4bbb0977b1c9d2dec5e45c22b23dc8
Thomas <whelan.thomas@gmail.com>
2021-10-16 17:43:25 +0900

    added google sheet api

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
 import requests
+from datetime import datetime
+
+GENDER = "male"
+WEIGHT_KG = "70"
+HEIGHT_CM = "170"
+AGE = "29"
 
 APP_ID = "f457de60"
 API_KEY = "d8ca4caad74c8306f867d7fbd21caa4c"
 
-nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = "https://api.sheety.co/39441e5d38b8d71a96bbb5417bbbeb42/workoutTracking/workouts"
+
+exercise_text = input("Tell me which exercises you did: ")
 
 headers = {
     "x-app-id": APP_ID,
     "x-app-key": API_KEY,
 }
 
-query = {
-    "query":"ran 20 miles",
-    "gender":"male",
-    "weight_kg":"50",
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
 }
 
-response = requests.post(url=nutritionix_endpoint, json=query, headers=headers)
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
 
-print(response.text)
+################### Start of Step 4 Solution ######################
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+
+    print(sheet_response.text)
